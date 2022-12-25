7a2b90d490b0ffd2fface6635d1c02b21a428960
Michael Paul <michael.paul.tlos@gmail.com>
2022-12-25 14:42:23 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const faders = document.querySelectorAll('.fade-in')
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+
+document.querySelectorAll(".nav-link"). forEach(n => n.addEventListener("click",() => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+
+const appearOptions = {
+    threshold: 0.3
+};
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        }
+        else {
+            entry.target.classList.add("appear");
+            appearOnScroll.unobserve(entry.target);
+        }
+    })
+}, appearOptions); 
+
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+})
